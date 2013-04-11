7055f269766714d3b5be8944520d0f6c164cdddb
Christopher Mitchell <christopher@granite.(none)>
2013-04-11 16:15:48 -0400

    Added Minecraft voxelization code

---
+++ kmz2mc.py
@@ -0,0 +1,429 @@
+#!/usr/bin/python
+import sys
+sys.path.append("../pymclevel/")
+import mclevel
+import collada
+import numpy as np
+from numpy import array
+import math
+import mcBlockData
+import scipy.optimize as scop
+
+epsilon = 1e-2
+
+class Tri2Voxel:
+
+	def __init__(self,model):
+		self.offset = array([0,0,0])	# Offset applied to incoming tris
+		self.scale  = array([1,1,1])	# Scaling applied to incoming tris
+		self.voffset= array([0,0,0])	# Offset of the origin voxel's vertex
+		self.arr3d_id = array([])		# Nice big array :P
+		self.arr3d_dt = array([])		# Nice big array :P
+		self.model = model
+		self.voxchg = 0
+		self.pxscan = 0
+
+	def geom_prep(self,mins,maxs):
+		self.offset = array([-mins[0],-mins[1],-mins[2]])
+		self.arrdim = (np.ceil((maxs[0]-mins[0])*self.scale[0]+1), \
+		               np.ceil((maxs[1]-mins[1])*self.scale[1]+1), \
+		               np.ceil((maxs[2]-mins[2])*self.scale[2]+1))
+		print("Reserving %d x %d x %d array..." % (self.arrdim[0], self.arrdim[1], self.arrdim[2]))
+		self.arr3d_id = np.zeros(self.arrdim)
+		self.arr3d_dt = np.zeros(self.arrdim)
+
+		self.tvoffset = array([self.offset,]*3)
+		self.tvscale  = array([self.scale,]*3)
+
+	''' Convert a triangle in 3-space
+	''  into voxels. arr3d is a reference
+	''  to a 3D array, triangle is the triangle
+	''  being converted, and offset and scale are
+	''  used to scale model coordinates to voxel
+	''  coordinates. Offset is applied BEFORE scale.
+	'''
+	def geom_tri2voxel(self,triangle):
+
+		tv = triangle.vertices
+		tt = triangle.texcoords
+		try:
+			tm = self.model.materials[triangle.material+"ID"]
+			ti = tm.effect.params[0].image
+		except:
+			tm = None
+			ti = None
+
+		if tv.shape != (3,3):
+			print("Warning: Bad triangle shape:")
+			print(tv.shape)
+			return
+
+		# Step 1: Offset the triangle in 3-space
+		# Step 2: Scale the triangle in 3-space
+		tv = tv + self.tvoffset
+		tv = tv * self.tvscale
+
+		# Step 3: Find the triangle's "dimensions" along each axis
+		xyzs = zip(*tv)
+		spanx = max(xyzs[0])-min(xyzs[0])
+		spany = max(xyzs[1])-min(xyzs[1])
+		spanz = max(xyzs[2])-min(xyzs[2])
+
+		# Step 4: Do some sort of preprocessing to find tm/ti/tt coordinates
+		txc = None
+		if tt and ti:
+			txs = ti.uintarray.shape
+			txc = np.dot(tt[0],array([[txs[1],0],[0,txs[0]]]))
+
+		# Step 5: Find volume the triangle lies inside
+		xrng = [self.geom_c2vcd(min(xyzs[0]),0), self.geom_c2vcu(max(xyzs[0]),0)]
+		yrng = [self.geom_c2vcd(min(xyzs[1]),1), self.geom_c2vcu(max(xyzs[1]),1)]
+		zrng = [self.geom_c2vcd(min(xyzs[2]),2), self.geom_c2vcu(max(xyzs[2]),2)]
+		xs = np.linspace(xrng[0],xrng[1],xrng[1]-xrng[0]+1)
+		ys = np.linspace(yrng[0],yrng[1],yrng[1]-yrng[0]+1)
+		zs = np.linspace(zrng[0],zrng[1],zrng[1]-zrng[0]+1)
+
+		# Step 6: Determine major, submajor, and minor axes
+		attempts = [0, 0, 0]
+		axismode = 0
+		axispass = False
+	
+		if spanz < spanx and spanz < spany:
+			axismode = 0
+		elif spany < spanx and spany < spanz:
+			axismode = 1
+		else:
+			axismode = 2
+
+		while not(axispass) and sum(attempts) < len(attempts):
+			attempts[axismode] = 1
+
+			if axismode == 0:
+				majax = 0
+				smjax = 1
+				minax = 2
+			elif axismode == 1:
+				majax = 0
+				smjax = 2
+				minax = 1
+			elif axismode == 2:
+				majax = 1
+				smjax = 2
+				minax = 0
+
+			if (tv[0,majax] == tv[1,majax] and tv[0,smjax] == tv[1,smjax]) or \
+			   (tv[1,majax] == tv[2,majax] and tv[1,smjax] == tv[2,smjax]) or \
+			   (tv[2,majax] == tv[0,majax] and tv[2,smjax] == tv[0,smjax]):
+				# Deal with degenerate triangle
+				print("***This triangle will be degenerate***")
+				axismode = (axismode+1)%3
+				# Fine-grained axis spacing
+				axisspace = 1 + max(xrng[1]-xrng[0],yrng[1]-yrng[0],zrng[1]-zrng[0])
+				xs = np.linspace(xrng[0],xrng[1],axisspace)
+				ys = np.linspace(yrng[0],yrng[1],axisspace)
+				zs = np.linspace(zrng[0],zrng[1],axisspace)
+			else:
+				axispass = True
+
+		# Set up the array of coordinates here in case we set fine-grained spacing
+		vol = [xs,ys,zs]
+
+		# Step 7: Perform the voxelization
+		voxstart = self.voxchg
+		# Iterate over the XY, XZ, or YZ plane
+		for fixa in [0.0, 0.1, -0.1, 0.2, -0.2, 0.3, -0.3]:
+			for fixb in [0.0, 0.1, -0.1, 0.2, -0.2, 0.3, -0.3]:
+				for ca in fixa + vol[majax]:
+					for cb in fixb + vol[smjax]:
+						x0 = ca if majax == 0 else None
+						y0 = ca if majax == 1 else (cb if smjax == 1 else None)
+						z0 = cb if smjax == 2 else None
+
+						x1, y1, z1, filled = self.geom_bary(tv, x0, y0, z0)
+
+						if filled:
+							mat, dat = self.geom_mat(tv,x0,y0,z0,ti,txc,majax,smjax,minax)
+							a = self.geom_vc2c(x1,0)
+							b = self.geom_vc2c(y1,1)
+							c = self.geom_vc2c(z1,2)
+							self.arr3d_id[a,b,c] = mat
+							self.arr3d_dt[a,b,c] = dat
+							self.voxchg += 1
+				if self.voxchg - voxstart > 0:
+					break
+			if self.voxchg - voxstart > 0:
+				break
+
+		if self.voxchg - voxstart == 0:
+			print("Warning: No voxels were set for this tri (thin tri issue?)")
+			print(tv)
+			print(vol)
+			print("-------")
+	
+	def geom_c2vcd(self,coord,axis):
+		return round(coord + self.voffset[axis]) - 0.5
+
+	def geom_c2vcu(self,coord,axis):
+		return round(coord + self.voffset[axis]) + 0.5
+
+	def geom_vc2c(self,coord,axis):
+		return np.round(coord - self.voffset[axis] - 0.5)
+
+	def geom_cart2bary_elf(self,tri,x,y,z):
+		if x == None:
+			a = 1
+			b = 2
+			c = 0
+		elif y == None:
+			a = 0
+			b = 2
+			c = 1
+		else:
+			a = 0
+			b = 1
+			c = 2
+
+		reindex = array([a,b,c])
+		trimatrix = np.matrix(tri[reindex]).transpose()  
+		#instead of trimatrix = matrix(tris).transpose()
+		P = np.matrix([[x],[y],[z]])[reindex]
+		#instead of P =matrix([[x],[y],[z]])
+
+		offset = trimatrix[:,0]
+		centered = trimatrix - offset # translate to origin
+		theta_x = math.atan2(centered[2,2],centered[1,2])
+		rx = np.matrix([[1,0,0],[0,math.cos(-theta_x),-math.sin(-theta_x)],[0,math.sin(-theta_x),math.cos(-theta_x)]])
+		intermediate1 = rx*centered
+		theta_z = math.atan2(intermediate1[1,2],intermediate1[0,2])
+		rz = np.matrix([[math.cos(-theta_z),-math.sin(-theta_z),0],[math.sin(-theta_z),math.cos(-theta_z),0],[0,0,1]])
+		intermediate2 = rz*intermediate1
+		theta_x2 = math.atan2(intermediate2[2,1],intermediate2[1,1])
+		rx2 = np.matrix([[1,0,0],[0,math.cos(-theta_x2),-math.sin(-theta_x2)],[0,math.sin(-theta_x2),math.cos(-theta_x2)]])
+		planar = rx2*intermediate2
+
+		P[2] = Unknown("z")
+		Pplanar = rx2*rz*rx*(P-offset) 
+
+		# Do something here to set tolerances
+		# and take care of rounding error, if it seems like it matters
+
+		T = np.matrix([[(planar[0,0]-planar[0,2]),(planar[0,1]-planar[0,2])],[(planar[1,0]-planar[1,2]),(planar[1,1]-planar[1,2])]])
+		det = np.linalg.det(T)
+		if det == 0:        # Incoming div-by-0
+			print("Determinant: Div-by-0 warning")
+			return [0., 0., 0., x, y, z]
+		lam12 = np.linalg.inv(T) * (Pplanar[:2]-planar[:2,2])
+		lambda1 = lam12[0,0]
+		lambda2 = lam12[1,0]
+		lambda3 = 1. - lambda1 - lambda2
+		rval = [lambda1,lambda2,lambda3,x,y,z]
+		rval[3+c] = lambda1*trimatrix[2,0]+lambda2*trimatrix[2,1]+lambda3*trimatrix[2,2]
+
+		# feel free to put better guesses if you have them
+		# but it's linear so I think it should be okay?
+		solved = (rval[3+c] - P[2,0]).collapse_on('z')
+		real_z = -solved.sterm / solved.mterm
+		if solved(z=real_z) < epsilon:
+			rval[0] = rval[0](z=real_z)
+			rval[1] = rval[1](z=real_z)
+			rval[2] = rval[2](z=real_z)
+			rval[3+c] = rval[3+c](z=real_z)
+		else:
+			print "Found no solution to equation %s=0" % (str(rval[3+c] - P[2,0]))
+			return [0., 0., 0., x, y, z]
+
+		return rval
+
+	def geom_cart2bary(self,tri,x,y,z):
+		P = array([x,y,z])
+		if x == None:
+			a = 1
+			b = 2
+			c = 0
+		elif y == None:
+			a = 0
+			b = 2
+			c = 1
+		else:
+			a = 0
+			b = 1
+			c = 2
+
+		det = (tri[1,b]-tri[2,b])*(tri[0,a]-tri[2,a]) + \
+		      (tri[2,a]-tri[1,a])*(tri[0,b]-tri[2,b])
+
+		if det != 0.:		# No incoming div-by-0
+			# Compute barycentric coordinates
+			l1 = ((tri[1,b]-tri[2,b])*(P[a]-tri[2,a]) + \
+				  (tri[2,a]-tri[1,a])*(P[b]-tri[2,b]))/det;
+			l2 = ((tri[2,b]-tri[0,b])*(P[a]-tri[2,a]) + \
+				  (tri[0,a]-tri[2,a])*(P[b]-tri[2,b]))/det;
+			l3 = 1. - l1 - l2
+
+		else:				# Incoming divide-by-zero.
+			print("Determinant: Div-by-0 warning %s %s %s" % (str(x),str(y),str(z)))
+			return [0., 0., 0., x, y, z]
+
+		rval = [l1, l2, l3, x, y, z]
+		rval[3+c] = (l1*tri[0,c]) + (l2*tri[1,c]) + (l3*tri[2,c])
+
+		return rval
+
+	def geom_bary(self,tri,x,y,z):
+
+
+		# Get the barycentric coordinates of the center of this quad
+		[l1, l2, l3, x, y, z] = self.geom_cart2bary(tri, x, y, z)
+		if l1 == 0. and l2 == 0. and l3 == 0.:
+			return [x, y, z, False]
+
+		inside = l1>=0 and l1<=1 and l2>=0 and l2<=1 and l3>=0 and l3<=1
+
+		# give back the results
+		return [x, y, z, inside]
+
+
+	def geom_mat(self,tri,x,y,z,timg,ttex,majax,smjax,minax):
+
+		if ttex == None or timg == None:
+			return [1, 0]
+
+		# Step 1: Grab all the points
+		p1 = [x, y, z]
+		p1[majax] -= 0.5
+		p1[smjax] -= 0.5
+		p1 = self.geom_cart2bary(tri, p1[0], p1[1], p1[2])
+		if p1[0] == 0 and p1[2] == 0 and p1[2] == 0:
+			return [1, 0]
+
+		p2 = [x, y, z]
+		p2[majax] += 0.5
+		p2[smjax] -= 0.5
+		p2 = self.geom_cart2bary(tri, p2[0], p2[1], p2[2])
+		if p2[0] == 0 and p2[2] == 0 and p2[2] == 0:
+			return [1, 0]
+
+		p3 = [x, y, z]
+		p3[majax] -= 0.5
+		p3[smjax] += 0.5
+		p3 = self.geom_cart2bary(tri, p3[0], p3[1], p3[2])
+		if p3[0] == 0 and p3[2] == 0 and p3[2] == 0:
+			return [1, 0]
+
+		p4 = [x, y, z]
+		p4[majax] += 0.5
+		p4[smjax] += 0.5
+		p4 = self.geom_cart2bary(tri, p4[0], p4[1], p4[2])
+		if p4[0] == 0 and p4[2] == 0 and p4[2] == 0:
+			return [1, 0]
+
+
+		# Step 2: Map modified texture coordinates to pixel coords
+		p1 = np.dot(p1[0:3],ttex)
+		p2 = np.dot(p2[0:3],ttex)
+		p3 = np.dot(p3[0:3],ttex)
+		p4 = np.dot(p4[0:3],ttex)
+
+		# Step 3: Scan/rasterize over all pixels
+		c = 1+abs(max(p2[0]-p1[0],p4[0]-p3[0]))
+		d = 1+abs(max(p3[1]-p1[1],p4[1]-p2[1]))
+
+		# Cheap trimming. Appears to be ok.
+		c = c if c < 7 else 7
+		d = d if d < 7 else 7
+
+		pxcount = 0
+		pxsum = [0, 0, 0]
+
+		for a in np.linspace(0,1,d):
+			for b in np.linspace(0,1,c):
+				cx = a*p3[0]+(1-a)*p1[0]+a*(p4[0]-p3[0])*b+(1-a)*(p2[0]-p1[0])*b
+				cy = a*p3[1]+(1-a)*p1[1]+a*(p4[1]-p3[1])*b+(1-a)*(p2[1]-p1[1])*b
+				# Step 3: Grab the pixel there
+				pxc = array([cx % timg.uintarray.shape[1], \
+		             (timg.uintarray.shape[0] - cy) % timg.uintarray.shape[0]])
+				if pxc[1] >= timg.uintarray.shape[0] or pxc[0] >= timg.uintarray.shape[1]:
+					continue
+				pixel = timg.uintarray[pxc[1],pxc[0]]
+				pxcount += 1
+				pxsum = [pxsum[0]+pixel[0], pxsum[1]+pixel[1], pxsum[2]+pixel[2]]
+
+		data, damage = [1, 0]
+		if pxcount > 0:
+			self.pxscan += pxcount
+			pixel = [float(pxsum[0])/float(pxcount), \
+					 float(pxsum[1])/float(pxcount), \
+					 float(pxsum[2])/float(pxcount)]
+			data, damage = mcBlockData.nearest(int(pixel[0]),int(pixel[1]),int(pixel[2]))
+		return [data, damage]
+
+def main():
+	if len(sys.argv) < 2:
+		print("Usage: %s <KMZ_file>" % sys.argv[0])
+		return
+
+	filename = sys.argv[1]
+	mesh = collada.Collada(filename, ignore=[collada.DaeUnsupportedError,
+	               collada.DaeBrokenRefError])
+
+	maxs = array([-1e99,-1e99,-1e99])
+	mins = array([ 1e99, 1e99, 1e99])
+	for geom in mesh.geometries:
+		for triset in geom.primitives:
+			maxs = array([max(maxs[0],np.max(triset.vertex[:,0])), \
+			              max(maxs[1],np.max(triset.vertex[:,1])), \
+			              max(maxs[2],np.max(triset.vertex[:,2]))])
+			mins = array([min(mins[0],np.min(triset.vertex[:,0])), \
+			              min(mins[1],np.min(triset.vertex[:,1])), \
+			              min(mins[2],np.min(triset.vertex[:,2]))])
+
+	# Get some sort of scaling information
+	scale = [.01,.01,.01]
+	if mesh.assetInfo != None and mesh.assetInfo.unitmeter != None:
+		print("This model contains units, %f %s per meter" % (mesh.assetInfo.unitmeter, mesh.assetInfo.unitname))
+		scale = mesh.assetInfo.unitmeter
+		scale = [scale, scale, scale]
+
+	t2v = Tri2Voxel(mesh)
+	t2v.scale = array(scale)
+	t2v.geom_prep(mins,maxs)
+
+	for geom in mesh.geometries:
+		for triset in geom.primitives:
+			trilist = list(triset)
+			for tri in trilist:
+				t2v.geom_tri2voxel(tri)
+
+	# Print some stats
+	ar1  = np.count_nonzero(t2v.arr3d_id)
+	ar01 = np.prod(t2v.arrdim)
+	print("%d/%d voxels filled (%.2f%% fill level)" % (ar1,ar01,100*ar1/ar01))
+	print("t2v reports %d voxels changed" % t2v.voxchg)
+	
+	# Paste into MC level
+	
+	level = mclevel.fromFile("/home/christopher/.minecraft/saves/Cemetech/level.dat")
+	t2v.arr3d_id = np.fliplr(t2v.arr3d_id)
+	t2v.arr3d_dt = np.fliplr(t2v.arr3d_dt)
+	for x in xrange(0,int(np.ceil(t2v.arrdim[0]/16.))):
+		for z in xrange(0,int(np.ceil(t2v.arrdim[1]/16.))):
+
+			chunk = level.getChunk(x,z)
+			xmax = min(16,t2v.arrdim[0]-16*x)
+			zmax = min(16,t2v.arrdim[1]-16*z)
+
+			chunk.Blocks[0:xmax,0:zmax,34:(34+t2v.arrdim[2])] = \
+			      t2v.arr3d_id[(16*x):(16*x+xmax),(16*z):(16*z+zmax),:]
+			chunk.Data[0:xmax,0:zmax,34:(34+t2v.arrdim[2])] = \
+			      t2v.arr3d_dt[(16*x):(16*x+xmax),(16*z):(16*z+zmax),:]
+
+			chunk.chunkChanged()
+
+	print("Relighting level...")
+	level.generateLights()
+	print("Saving level...")
+	level.saveInPlace()
+
+# Get running!
+if __name__ == '__main__':
+	main()
